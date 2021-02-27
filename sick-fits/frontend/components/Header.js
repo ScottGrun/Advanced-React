007e6afef8ae71e6a9c66af3acb840732d5837e1
Scott Grunerud <sgrunerud@edu.uwaterloo.ca>
2021-02-27 17:44:13 -0500

    Added global styles

--- sick-fits/frontend/components/Header.js
+++ sick-fits/frontend/components/Header.js
@@ -1,18 +1,54 @@
 import React from 'react';
+import styled from 'styled-components';
 
+// Components
 import Link from 'next/link';
 import Nav from './Nav';
 
+// Styled Components
+const Logo = styled.h1`
+  background-color: red;
+  font-size: 4rem;
+  margin-left: 2rem;
+  z-index: 2;
+  transform: skew(-7deg);
+
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem;
+  }
+`;
+
+const HeaderStyled = styled.header`
+  .bar {
+    border-bottom: 10px solid var(--black, black);
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1px solid var(--black, black);
+  }
+`;
+
 export default function Header() {
   return (
-    <header>
+    <HeaderStyled>
       <div className="bar">
-        <Link href="/">Sick Fits</Link>
+        <Logo>
+          <Link href="/">Sick Fits</Link>
+        </Logo>
       </div>
       <div className="sub-bar">
         <p>Search</p>
       </div>
       <Nav />
-    </header>
+    </HeaderStyled>
   );
 }
--- sick-fits/frontend/components/Page.js
+++ sick-fits/frontend/components/Page.js
@@ -1,12 +1,68 @@
 import React from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
 import Header from './Header';
+
+const GlobalStyles = createGlobalStyle`
+
+@font-face {
+  font-family: 'radnika_next';
+  src: url('../public/static/radnikanext-medium-webfont.woff2');
+  format: 'woff2';
+  font-weight: normal;
+  font-style: normal;
+}
+
+:root {
+  --red: #ff0000;
+  --black: #393939;
+  --grey: #3A3A3A;
+  --light-grey: #e1e1e1;
+  --off-white: #ededed;
+  --max-width: 1000px;
+  --box-shadow: 0 12px 23px 0 rgba(0, 0, 0, .09)
+}
+
+html{
+  box-sizing: border-box;
+}
+
+*,*:before, *:after{
+  box-sizing: inherit;
+}
+
+body{
+  font-family: 'radnika_next', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  padding: 0;
+  margin: 0;
+  font-size: 1.5rem;
+}
+
+a {
+  text-decoration: none;
+  color: var(--black);
+}
+
+a:hover{
+  text-decoration: underline;
+}
+
+button{
+  font-family: 'radnika_next', --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+`;
+
+const InnerStyles = styled.div`
+  max-width: var(--max-width);
+  margin: 0 auto;
+  padding: 2rem;
+`;
 
 export default function Page({ children }) {
   return (
     <div>
+      <GlobalStyles />
       <Header />
-      <p>I am the page</p>
-      {children}
+      <InnerStyles>{children}</InnerStyles>
     </div>
   );
 }
