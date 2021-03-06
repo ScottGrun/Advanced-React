daa3eb2ab49a549b55fab4716f5d9c4850a91fd6
Scott Grunerud <sgrunerud@edu.uwaterloo.ca>
2021-03-06 12:17:36 -0500

    Added product schema

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,50 +1,59 @@
-import { config, createSchema } from '@keystone-next/keystone/schema';
-import { User } from './schemas/User';
 import { createAuth } from '@keystone-next/auth';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+import { Product } from './schemas/Product';
+import { User } from './schemas/User';
 import 'dotenv/config';
-import { withItemData, statelessSessions } from '@keystone-next/keystone/session'
-const databaseURL = process.env.DATABASE_URL;
+
+const databaseURL =
+  process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
 
 const sessionConfig = {
-    maxAge: 60 * 60 * 24 * 360,// Session valid duration
-    secret: process.env.COOKIE_SECRET,
-}
+  maxAge: 60 * 60 * 24 * 360, // How long they stay signed in?
+  secret: process.env.COOKIE_SECRET,
+};
 
 const { withAuth } = createAuth({
-    listKey: 'User',
-    identityField: 'email',
-    secretField: 'password',
-    initFirstItem: {
-        fields: ['name', 'email', 'password']
-        // TODO: Add in inital roles
-    }
-})
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TODO: Add in inital roles here
+  },
+});
 
-export default withAuth(config({
+export default withAuth(
+  config({
+    // @ts-ignore
     server: {
-        cors: {
-            origin: [process.env.FRONTEND_URL],
-            credentials: true
-        },
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
     },
     db: {
-        adapter: 'mongoose',
-        url: databaseURL
-        // TODO: Add data seeding here
+      adapter: 'mongoose',
+      url: databaseURL,
+      // TODO: Add data seeding here
     },
     lists: createSchema({
-        // Schema items here
-        User
+      // Schema items go in here
+      User,
+      Product,
     }),
     ui: {
-        //Show UI for only authed users
-        isAccessAllowed: ({ session }) => {
-            console.log(session);
-            return !!session?.data;
-        }
+      // Show the UI only for poeple who pass this test
+      isAccessAllowed: ({ session }) =>
+        // console.log(session);
+        !!session?.data,
     },
     session: withItemData(statelessSessions(sessionConfig), {
-        // Graphql query 
-        User: 'id'
-    })
-}));
+      // GraphQL Query
+      User: 'id name email',
+    }),
+  })
+);
