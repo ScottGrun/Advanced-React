2dd7529883e93e58fedc2fabefdd53daefc04e0c
Scott Grunerud <sgrunerud@edu.uwaterloo.ca>
2021-03-21 13:19:39 -0400

    Added sign in and out functionality, fixed data not updating in document head

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,15 +1,27 @@
-import React from 'react';
 import Link from 'next/link';
-
 import NavStyles from './styles/NavStyles';
+import { useUser } from '../hooks/useUser';
+import SignOut from './SignOut';
 
 export default function Nav() {
+  const user = useUser();
+  console.log(user);
   return (
     <NavStyles>
       <Link href="/products">Products</Link>
-      <Link href="/sell">Sell</Link>
-      <Link href="/orders">Orders</Link>
-      <Link href="/account">Account</Link>
+      {user && (
+        <>
+          <Link href="/sell">Sell</Link>
+          <Link href="/orders">Orders</Link>
+          <Link href="/account">Account</Link>
+          <SignOut />
+        </>
+      )}
+      {!user && (
+        <>
+          <Link href="/signin">Sign In</Link>
+        </>
+      )}
     </NavStyles>
   );
 }
--- stepped-solutions/37/Nav.js
+++ stepped-solutions/37/Nav.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import SignOut from '../../sick-fits/frontend/components/SignOut';
 import NavStyles from './styles/NavStyles';
 import { useUser } from './User';
 
